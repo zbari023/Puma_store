94b26e05cfa64d04d75245143d692f0d3e1a57ce
zbari023 <barry450643@gmail.com>
2023-07-23 21:57:59 +0200

    add in view file

--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def store(request):
+    return render(request,'store/store.html',{})
+
+
+def cart(request):
+    return render(request,'store/cart.html',{})
+
+
+def checkout(request):
+    return render(request,'store/checkout.html',{})
