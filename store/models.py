9f21515bef54715435d1ddcbb2fbc69d13e5364d
zbari023 <barry450643@gmail.com>
2023-07-24 13:01:12 +0200

    customization of Admin page and add somr products

--- store/models.py
+++ store/models.py
@@ -39,3 +39,17 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0 ,null=True , blank=True )
     date_added = models.DateTimeField(auto_now_add=True)
     
+    
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True ,blank=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True ,blank=True)
+    address = models.CharField(max_length=50, null=True)
+    state = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=50, null=True)
+    zipcode = models.CharField(max_length=50, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
+    
+    
