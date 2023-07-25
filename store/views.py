f92e8d02a701bb88604cf96525177a51d237f770
zbari023 <barry450643@gmail.com>
2023-07-25 13:33:24 +0200

    change the data in checkout html

--- store/views.py
+++ store/views.py
@@ -21,4 +21,12 @@ def cart(request):
 
 
 def checkout(request):
-    return render(request,'store/checkout.html',{})
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order , created = Order.objects.get_or_create(customer=customer , complete=False)
+        items = order.orderitem_set.all()
+    
+    else:
+        items = []
+        order = {'get_cart_total':0 , 'get_cart_items':0}
+    return render(request,'store/checkout.html',{'items':items , 'order':order})
